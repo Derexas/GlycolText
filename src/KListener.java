807d37bb7fb344fa11a2836e4c1df180a9a26def
derexas <derexas@gmail.com>
2017-04-07 18:15:54 +0200

    0

--- src/KListener.java
+++ src/KListener.java
@@ -12,7 +12,9 @@ public class KListener implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		System.out.println("keyPressed="+KeyEvent.getKeyText(e.getKeyCode()));
+		String text = KeyEvent.getKeyText(e.getKeyCode());
+		System.out.println("keyPressed="+text);
+		core.addText(text);
 	}
 
 	@Override
