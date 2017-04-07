7094163782c97c873d4db1266270003a9a35dded
derexas <derexas@gmail.com>
2017-04-07 18:06:42 +0200

    0

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,7 @@
-import java.awt.EventQueue;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.util.ArrayList;
-
-import java.util.Scanner;
 
 import javax.swing.JFrame;
 
 public class Main {
-	
-	private static ArrayList<Character> text = new ArrayList<Character>();
-	
-	public static void addText(String s)
-	{
-		for (char c : s.toCharArray())
-			text.add(c);
-	}
-	
-	public static void clear(String s)
-	{
-		for (int i = 0; i < 5; i++)
-			System.out.println("\n");
-	}
 	
 	public static void initJFrame()
 	{
