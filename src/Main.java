8e767bfecd465d9c76269fac2d4270ae558c9fc0
derexas <derexas@gmail.com>
2017-05-22 09:44:52 +0200

    memento part pretty much done

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,11 @@
 import javax.swing.JFrame;
 
 public class Main {
-	
-	public static void initJFrame()
-	{
-		/*JFrame f = new JFrame();
-		f.setTitle("Simple example");
-        f.setSize(300, 200);
-        f.setLocationRelativeTo(null);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        SPanel sp = new SPanel();
-    	KListener listener = new KListener();
-    	sp.addKeyListener(listener);
-		f.add(sp);
-		f.setVisible(true);*/
-		new GUI();
-	}
 
 	public static void main(String[] args)
 	{		
-		initJFrame();
+		Core core = new Core();
+		new GUI(core);
 	}
 
 }
